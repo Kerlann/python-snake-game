52af0bce0d9673cd310589dc199c2f3c421bc5e7
Kerlann <34661926+Kerlann@users.noreply.github.com>
2025-03-24 10:52:42 +0100

    Ajout du fichier de constantes pour le jeu

---
+++ src/utils/constants.py
@@ -0,0 +1,26 @@
+"""
+Constantes utilisées dans le jeu Snake.
+"""
+
+# Dimensions de la fenêtre
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Couleurs (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARK_GREEN = (0, 100, 0)
+LIGHT_GREEN = (144, 238, 144)
+
+# Paramètres du jeu
+FPS = 60
+GRID_SIZE = 20
+GRID_WIDTH = WINDOW_WIDTH // GRID_SIZE
+GRID_HEIGHT = WINDOW_HEIGHT // GRID_SIZE
+SNAKE_SPEED = 10  # cases par seconde
+
+# Titres
+GAME_TITLE = "Snake Game"
