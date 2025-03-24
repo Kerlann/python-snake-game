671af9e47f1c298f9e027d8cb6756566fd2da274
Kerlann <34661926+Kerlann@users.noreply.github.com>
2025-03-24 10:48:37 +0100

    Ajout du fichier principal main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Jeu Snake en Python avec Pygame
+Point d'entrée principal du programme
+"""
+
+if __name__ == "__main__":
+    print("Initialisation du jeu Snake...")
+    # Le code sera implémenté au fur et à mesure du développement
